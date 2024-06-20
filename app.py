0d438a018f27e6066d364ad8dad9f24ebac895d6
Dmitry <152660440+Kr10NI@users.noreply.github.com>
2024-06-20 21:10:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,14 @@ import telebot
 from telebot import types
 import requests
 import random
-
+import psycopg2
+from contextlib import closing
+with closing(psycopg2.connect(dbname='films_bot', user='projectuser', password='password', host='localhost')) as conn:
+    with conn.cursor() as cursor:
+        cursor.execute('SELECT * FROM filmlist ')
+        for row in cursor:
+            print(row) 
+            # проверка работы бд
 bot = telebot.TeleBot('7390621130:AAGaN0szPdQb2dFSxGwJargEDUcSnZggxI0')
 api_key = "66c4bb59-6228-4ba6-a93e-90506aeb9bfe"
 api_url = "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=1"
@@ -14,6 +21,7 @@ headers = {
 
 films_data = []
 watch_later_list = []
+searched_films = []
 
 def load_top_films():
     global films_data
@@ -43,11 +51,8 @@ def start(message):
     markup.add(button6)
     markup.add(button4, button5)
 
-    bot.send_message(
-        message.chat.id,
-        f'Привет! Это MyHobbyList! \n \nДавай подберем тебе что-нибудь интересного!',
-        reply_markup=markup
-    )
+    user_first_name = str(message.chat.first_name)
+    bot.reply_to(message, f"Привет, {user_first_name}", reply_markup = markup)
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback(call):
@@ -97,6 +102,8 @@ def callback(call):
             msg = bot.send_message(call.message.chat.id, "Введите название фильма:")
             bot.register_next_step_handler(msg, search_film_by_keyword)
 
+        elif call.data == 'information_surprise':
+            get_film_details(call.message)
 def display_top_films(message):
     top_films = ""
     for i, film in enumerate(films_data, start=1):
@@ -116,7 +123,7 @@ def process_film_number(message):
         film_index = int(message.text) - 1
         if 0 <= film_index < len(films_data):
             film = films_data[film_index]
-            film_details = get_film_details(film, message.chat.id)  # Pass the chat ID
+            film_details = get_film_details(film, message.chat.id)
             bot.send_message(message.chat.id, film_details)
 
             backButton = types.InlineKeyboardButton("Назад", callback_data='back_to_film_list')
@@ -154,7 +161,7 @@ def get_film_details(film, chat_id):
             f"Возрастное ограничение: {data.get('ratingAgeLimits', 'N/A')}\n"
         )
         if 'posterUrl' in data:
-            bot.send_photo(chat_id=chat_id, photo=data['posterUrl'])
+            bot.send_photo(chat_id = chat_id, photo=data['posterUrl'])
         return film_details
     else:
         return "Не удалось получить подробную информацию о фильме."
@@ -178,24 +185,25 @@ def display_watch_later(message):
 
 def add_to_watch_later(call):
     film_index = int(call.data.split('_')[-1])
-    film = films_data[film_index]
+    if "searched" in call.data:
+        film = searched_films[film_index]
+    else:
+        film = films_data[film_index]
     watch_later_list.append(film)
     bot.send_message(call.message.chat.id, f"Фильм '{film['nameRu']}' добавлен в список 'Посмотреть позже'.")
-    button_back_film = types.InlineKeyboardButton("Вернуться к топ фильмам", callback_data="back_to_film_list")
-    button_back = types.InlineKeyboardButton("Назад", callback_data="back_to_menu")
-    markup = types.InlineKeyboardMarkup()
-    markup.add(button_back)
-    markup.add(button_back_film)
-    bot.send_message(call.message.chat.id, "Выберите действие:", reply_markup=markup)
 
 def random_film(message):
     if films_data:
         film = random.choice(films_data)
         film_index = films_data.index(film)
-        bot.send_message(message.chat.id, f"Советую посмотреть: {film['nameRu']} ({film['year']})")
+        genres = ", ".join([genre['genre'] for genre in film['genres']])
+        countries = ", ".join([country['country'] for country in film['countries']])
+        bot.send_message(message.chat.id, f"Советую посмотреть: {film['nameRu']}\nГод: {film['year']}\nЖанры: {genres}\nСтраны: {countries}")
         button_back = types.InlineKeyboardButton("Назад", callback_data="back_to_menu")
+        button_information = types.InlineKeyboardButton("Узнать подробнее", callback_data="information_surprise")
         button_add_to_watch_later = types.InlineKeyboardButton("Добавить в посмотреть позже", callback_data=f"add_to_watch_later_{film_index}")
         markup = types.InlineKeyboardMarkup()
+        markup.add(button_information)
         markup.add(button_add_to_watch_later)
         markup.add(button_back)
         bot.send_message(message.chat.id, "Выберите действие:", reply_markup=markup)
@@ -208,19 +216,20 @@ def random_film(message):
 
 def search_film_by_keyword(message):
     keyword = message.text
+    global searched_films
     response = requests.get(f"{search_api_url}?keyword={keyword}", headers=headers)
     if response.status_code == 200:
         data = response.json()
-        films = data.get('films', [])
-        if films:
+        searched_films = data.get('films', [])
+        if searched_films:
             search_results = ""
-            for i, film in enumerate(films[:10], start=1):
+            for i, film in enumerate(searched_films[:10], start=1):
                 genres = ", ".join([genre['genre'] for genre in film['genres']])
                 countries = ", ".join([country['country'] for country in film['countries']])
                 search_results += f"{i}. {film['nameRu']}\nГод: {film['year']}\nЖанры: {genres}\nСтраны: {countries}\n\n"
             bot.send_message(message.chat.id, search_results)
             msg = bot.send_message(message.chat.id, "Введите номер фильма из списка для получения подробной информации:")
-            bot.register_next_step_handler(msg, process_search_film_number, films)
+            bot.register_next_step_handler(msg, process_search_film_number)
         else:
             bot.send_message(message.chat.id, "Фильмы не найдены. Попробуйте другой запрос.")
             start(message)
@@ -228,17 +237,17 @@ def search_film_by_keyword(message):
         bot.send_message(message.chat.id, "Не удалось выполнить поиск фильма. Попробуйте позже.")
         start(message)
 
-def process_search_film_number(message, films):
+
+def process_search_film_number(message):
     try:
         film_index = int(message.text) - 1
-        if 0 <= film_index < len(films):
-            film = films[film_index]
-            film_details = get_film_details(film, message.chat.id)  # Pass the chat ID
+        if 0 <= film_index < len(searched_films):
+            film = searched_films[film_index]
+            film_details = get_film_details(film, message.chat.id)
             bot.send_message(message.chat.id, film_details)
 
             backButton = types.InlineKeyboardButton("Назад", callback_data='back_to_menu')
-            watchLaterButton = types.InlineKeyboardButton("Добавить в Посмотреть позже",
-                                                          callback_data=f'add_to_watch_later_{film_index}')
+            watchLaterButton = types.InlineKeyboardButton("Добавить в Посмотреть позже", callback_data=f'add_to_watch_later_searched_{film_index}')
             markup = types.InlineKeyboardMarkup()
             markup.add(backButton, watchLaterButton)
             bot.send_message(message.chat.id, "Выберите действие:", reply_markup=markup)
